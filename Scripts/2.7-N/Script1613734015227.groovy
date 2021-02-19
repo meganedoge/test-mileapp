d1afa8b4305d145d48844c35dcf6bfb6662f57ee
titus.baskara <titus.baskara@aprisma.co.id>
2021-02-19 20:03:52 +0700

    update test case

---
+++ Scripts/2.7-N/Script1613734015227.groovy
@@ -0,0 +1,29 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.callTestCase(findTestCase('fragment/access_loginUser'), [('orgName') : findTestData('data_login').getValue(1, 1)], 
+    FailureHandling.STOP_ON_FAILURE)
+
+WebUI.setText(findTestObject('Page_Mile.app  Login/textbox_usernameEmail'), '')
+
+WebUI.setText(findTestObject('Page_Mile.app  Login/textbox_password'), findTestData('data_login').getValue(3, 1))
+
+WebUI.click(findTestObject('Page_Mile.app  Login/button_loginUser'))
+
+WebUI.verifyTextPresent(findTestData('dat_warning').getValue(1, 4), false)
+
---
+++ Test Suites/Test_login.ts
@@ -0,0 +1,86 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test_login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2957d3fd-707e-4e7e-bc5d-c1d843fb7d06</testSuiteGuid>
+   <testCaseLink>
+      <guid>0ce5a8d6-73a5-468d-b5fa-ca0b1d0fae80</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1-P</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a0b74fdf-8236-4ded-9aa2-db4ed85859f9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1.1-N</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>aedeb04c-8cfa-4b0f-a69a-4c6fd5c934fa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/1.2.-N</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bee2ee0a-864d-49ed-8af9-f5d4f7a72968</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2-P</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0d2eef99-31d1-49c2-9d2f-12553b112ea5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2.1-N</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a7eb6916-d91f-4814-9bcb-a818222e83f8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2.2-N</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7529e24a-7a0c-4980-a2e5-1c49c3ff149e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2.3-N</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>61e72ba0-6ab4-44f3-beb0-1bd6efa378af</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2.4-N</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ad19ead6-ece9-45a3-9145-26144a10fdb7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2.5-N</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9e9118dd-26e4-4fc4-961e-50d8a4b56112</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2.6-N</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0aab7ea0-810b-45b3-8ba7-3f01259ea460</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2.7-N</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ad846eb0-b41c-408e-9685-1cca177ea2d2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/2.8-N</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
